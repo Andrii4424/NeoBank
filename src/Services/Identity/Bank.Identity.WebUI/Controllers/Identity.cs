43be763c623b228dcc7f8c6d94e9f37ac30d8a35
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-08-19 13:13:52 +0300

    Add Sign up and sign in pages

--- src/Services/Identity/Bank.Identity.WebUI/Controllers/Identity.cs
+++ src/Services/Identity/Bank.Identity.WebUI/Controllers/Identity.cs
@@ -5,9 +5,16 @@ namespace Bank.Identity.WebUI.Controllers
     public class Identity : Controller
     {
         [Route("/")]
-        public IActionResult Login()
+        public IActionResult SignIn()
         {
-            ViewBag.Title = "Login";
+            ViewBag.Title = "Sign In";
+            return View();
+        }
+
+        [Route("/sign-up")]
+        public IActionResult SignUp()
+        {
+            ViewBag.Title = "Sign Up";
             return View();
         }
     }
