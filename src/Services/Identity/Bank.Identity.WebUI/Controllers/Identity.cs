a5a2cc7bf92914fa92a9b456ba00c55162589dc9
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-08-19 09:26:05 +0300

    Add basic UI and update configuration

---
+++ src/Services/Identity/Bank.Identity.WebUI/Controllers/Identity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.Identity.WebUI.Controllers
+{
+    public class Identity : Controller
+    {
+        [Route("/")]
+        public IActionResult Login()
+        {
+            ViewBag.Title = "Login";
+            return View();
+        }
+    }
+}
---
+++ src/Services/Identity/Bank.Identity.WebUI/StartupServicesInjection/AddIdentityServices.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Bank.Identity.WebUI.StartupServicesInjection
+{
+    public static class AddApplicationServices
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllersWithViews()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+
+            return services;
+        }
+
+    }
+}
