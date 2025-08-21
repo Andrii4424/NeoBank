797c805b4825a4a3cabc6aa891af0df508014c11
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-08-21 15:55:22 +0300

    Update identity logic

--- src/Services/Identity/Identity.Server/Program.cs
+++ src/Services/Identity/Identity.Server/Program.cs
@@ -1,5 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+builder.Configuration
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables();
+
 
 
 app.Run();
