7a730e32c67a6de260de225737b8daba71436646
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-08-23 11:28:33 +0300

    Add dependencies and update configuration in WebUI

--- src/Services/Bank/API/Bank.API.WebUI/Program.cs
+++ src/Services/Bank/API/Bank.API.WebUI/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseHttpsRedirection();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+app.MapControllers();
+app.UseRouting();
 
 app.Run();
---
+++ src/Services/Bank/API/Bank.API.WebUI/StartupServicesInjection/AddApplicationServices.cs
@@ -0,0 +1,13 @@
+﻿namespace Bank.API.WebUI.StartupServicesInjection
+{
+    public static class AddApplicationServices
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+
+
+            return services;
+        }
+    }
+}
