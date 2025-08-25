03a8e1ee35f9fb36dcd06ce4c807c06fd42b608c
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-08-25 10:41:36 +0300

    Update bank ID type

---
+++ src/Services/Bank/API/Bank.API.Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.API.Infrastructure.Repository
+{
+    public class GenericRepository<T> where T : class
+    {
+
+    }
+}
