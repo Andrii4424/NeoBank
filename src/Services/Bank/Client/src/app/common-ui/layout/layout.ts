dbde2a7eb7b076a009119453dd5b3a627550c6fb
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-09-01 12:07:24 +0300

    Add labtop adaptivity

--- src/Services/Bank/Client/src/app/common-ui/layout/layout.ts
+++ src/Services/Bank/Client/src/app/common-ui/layout/layout.ts
@@ -14,10 +14,11 @@ import { BreakpointObserver } from '@angular/cdk/layout';
 export class Layout {
   //menu animation
   compact = false;
+  adaptive = false; //need to ignore scroll compact animation if it's labtop or mobile
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    if(pageYOffset > 440) {
+    if(pageYOffset > 440 && !this.adaptive) {
       this.compact = true;
     }
     else {
@@ -31,6 +32,7 @@ export class Layout {
   mobile = signal(false);
   @ViewChild('openMenu') openMenu!: ElementRef<HTMLImageElement> ;
   @ViewChild('closeMenu') closeMenu!: ElementRef<HTMLImageElement> ;
+  @ViewChild('adaptiveMenu') adaptiveMenu!: ElementRef<HTMLElement> ;
 
   @HostListener('window:click', ['$event'])
   onWindowClick(event: Event) {
@@ -38,10 +40,12 @@ export class Layout {
     else if (this.openMenu.nativeElement.contains(event.target as Node)) {
       this.openMenu.nativeElement.classList.remove('chosen');
       this.closeMenu.nativeElement.classList.add('chosen');
+      this.adaptiveMenu.nativeElement.classList.add('opened');
     }
     else if (this.closeMenu.nativeElement.contains(event.target as Node)) {
       this.openMenu.nativeElement.classList.add('chosen');
       this.closeMenu.nativeElement.classList.remove('chosen');
+      this.adaptiveMenu.nativeElement.classList.remove('opened');
     }
   }
 
@@ -49,6 +53,8 @@ export class Layout {
   constructor() {
     this.bo.observe(['(max-width: 1440px)']).subscribe(result => {
       this.labtop.set(result.matches);
+      if(result.matches) this.adaptive = true;
+      else this.adaptive = false;
     });
 
     this.bo.observe(['(max-width: 768px)']).subscribe(result => {
