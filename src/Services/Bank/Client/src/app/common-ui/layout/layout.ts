88f4a66be4cf7db372177360b4d136b7775a7df0
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-09-01 10:20:34 +0300

    Update adaptivity

--- src/Services/Bank/Client/src/app/common-ui/layout/layout.ts
+++ src/Services/Bank/Client/src/app/common-ui/layout/layout.ts
@@ -1,6 +1,7 @@
-import { Component, Host, HostListener } from '@angular/core';
+import { Component, ElementRef, Host, HostListener, inject, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from "../footer/footer";
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-layout',
@@ -11,7 +12,7 @@ import { Footer } from "../footer/footer";
 
 
 export class Layout {
-
+  //menu animation
   compact = false;
 
   @HostListener('window:scroll', [])
@@ -24,4 +25,36 @@ export class Layout {
       this.compact = false;
     }
   }
+
+  //adaptive
+  private bo = inject(BreakpointObserver);
+  labtop = signal(false);
+  mobile = signal(false);
+  @ViewChild('openMenu') openMenu!: ElementRef<HTMLImageElement> ;
+  @ViewChild('closeMenu') closeMenu!: ElementRef<HTMLImageElement> ;
+
+  @HostListener('window:click', ['$event'])
+  onWindowClick(event: Event) {
+    if(!(event.target instanceof Node)) return;
+    else if (this.openMenu.nativeElement.contains(event.target as Node)) {
+      this.openMenu.nativeElement.classList.remove('chosen');
+      this.closeMenu.nativeElement.classList.add('chosen');
+    }
+    else if (this.closeMenu.nativeElement.contains(event.target as Node)) {
+      this.openMenu.nativeElement.classList.add('chosen');
+      this.closeMenu.nativeElement.classList.remove('chosen');
+    }
+  }
+
+
+  constructor() {
+    this.bo.observe(['(max-width: 1440px)']).subscribe(result => {
+      this.labtop.set(result.matches);
+    });
+
+    this.bo.observe(['(max-width: 768px)']).subscribe(result => {
+      this.mobile.set(result.matches);
+    });
+  }
+
 }
