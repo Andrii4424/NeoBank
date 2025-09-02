5d14d925431dcd1323a3257075515a237040fe10
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-09-02 08:25:10 +0300

    Add sign-up page

--- src/Services/Bank/Client/src/app/app.routes.ts
+++ src/Services/Bank/Client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Layout } from './common-ui/layout/layout';
 import { BankInfo } from './bank-info/bank-info';
 import { Home } from './pages/home/home';
 import { Login } from './pages/auth/login/login';
+import { Register } from './pages/auth/register/register';
 
 export const routes: Routes = [
     {path: '', component: Layout, children: [
         {path: '', component: Home}
     ]},
-    {path: 'login', component: Login}
+    {path: 'login', component: Login},
+    {path: 'signup', component: Register}
 
 ];
---
+++ src/Services/Bank/Client/src/app/pages/auth/register/register.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Footer } from "../../../common-ui/footer/footer";
+
+@Component({
+  selector: 'app-register',
+  imports: [Footer],
+  templateUrl: './register.html',
+  styleUrl: './register.scss'
+})
+export class Register {
+
+}
