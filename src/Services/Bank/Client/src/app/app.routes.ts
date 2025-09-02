675a60ba45b58a69200da97b88d20affff915175
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-09-02 08:18:01 +0300

    Add login page

--- src/Services/Bank/Client/src/app/app.routes.ts
+++ src/Services/Bank/Client/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { Layout } from './common-ui/layout/layout';
 import { BankInfo } from './bank-info/bank-info';
+import { Home } from './pages/home/home';
+import { Login } from './pages/auth/login/login';
 
 export const routes: Routes = [
-    {path: '', component: Layout},
-    {path: 'BankInfo', component: BankInfo}
+    {path: '', component: Layout, children: [
+        {path: '', component: Home}
+    ]},
+    {path: 'login', component: Login}
 
 ];
---
+++ src/Services/Bank/Client/src/app/pages/auth/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Footer } from "../../../common-ui/footer/footer";
+
+@Component({
+  selector: 'app-login',
+  imports: [Footer],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+
+}
