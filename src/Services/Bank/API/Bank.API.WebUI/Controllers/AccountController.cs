ccc3c9eb3a5ecfb93953d72693fbbbaafcc31f2c
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-09-12 16:26:51 +0300

    Fix bug with login response

--- src/Services/Bank/API/Bank.API.WebUI/Controllers/AccountController.cs
+++ src/Services/Bank/API/Bank.API.WebUI/Controllers/AccountController.cs
@@ -43,7 +43,7 @@ namespace Bank.API.WebUI.Controllers
         {
             if(await _identityService.IsEmailUniqueAsync(loginDto.Email))
             {
-                return Unauthorized("Wrong email or password!");
+                return BadRequest("Wrong email or password!");
             }
             bool passwordResult = await _userManager.CheckPasswordAsync(await _identityService.GetUserByEmailAsync(loginDto.Email), loginDto.Password);
 
@@ -55,7 +55,7 @@ namespace Bank.API.WebUI.Controllers
             }
             else
             {
-                return Unauthorized("Wrong email or password!");
+                return BadRequest("Wrong email or password!");
             }
         }
 
