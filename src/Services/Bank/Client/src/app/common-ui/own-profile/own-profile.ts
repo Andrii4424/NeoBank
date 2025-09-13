457b25a50c44c4307f15b70857eae6f94cdf85ee
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-09-13 19:14:26 +0300

    Add own profile component

---
+++ src/Services/Bank/Client/src/app/common-ui/own-profile/own-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-own-profile',
+  imports: [],
+  templateUrl: './own-profile.html',
+  styleUrl: './own-profile.scss'
+})
+export class OwnProfile {
+
+}
