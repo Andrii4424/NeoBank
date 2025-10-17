b35594d2514fd8ed855f2224c03eca62bee702e3
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-10-17 17:09:35 +0300

    Add localization

--- src/Client/src/app/app.config.ts
+++ src/Client/src/app/app.config.ts
@@ -1,16 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authTokenInterceptor } from './auth/auth.interceptor';
 import { cookieInterceptor } from './auth/cookie.interceptor';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { provideTranslateHttpLoader, TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([cookieInterceptor, authTokenInterceptor]))
+    provideHttpClient(withInterceptors([cookieInterceptor, authTokenInterceptor])),
+    
+    importProvidersFrom(TranslateModule.forRoot({
+          defaultLanguage: 'en'
+        })),
+
+    provideTranslateHttpLoader({
+      prefix: './assets/i18n/',
+          suffix: '.json'
+    }),
   ]
 };
--- src/Client/src/app/common-ui/layout/layout.ts
+++ src/Client/src/app/common-ui/layout/layout.ts
@@ -1,13 +1,14 @@
-import { Component, ElementRef, Host, HostListener, inject, signal, ViewChild, ViewChildren } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Host, HostListener, inject, signal, ViewChild, ViewChildren } from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { Footer } from "../footer/footer";
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { SharedService } from '../../data/services/shared-service';
 import { OwnProfile } from "../own-profile/own-profile";
+import { FormsModule } from '@angular/forms';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, Footer, RouterLink, OwnProfile, RouterModule],
+  imports: [RouterOutlet, Footer, RouterLink, OwnProfile, RouterModule, FormsModule, TranslateModule],
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
@@ -16,17 +17,11 @@ import { OwnProfile } from "../own-profile/own-profile";
 export class Layout {
   //menu animation
   compact = false;
+  chosenLanguage: string | null=null;
   adaptive = false; //need to ignore scroll compact animation if it's labtop or mobile
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    if(pageYOffset > 440 && !this.adaptive) {
-      this.compact = true;
-    }
-    else {
-      this.compact = false;
-    }
-  }
+  showLanguageWindow = signal<boolean>(false);
+  @ViewChild('languageForm') languageForm!: ElementRef<HTMLElement> ;
+  @ViewChild('openLanguageWindow') openLanguageWindow!: ElementRef<HTMLElement> ;
 
   //adaptive
   private bo = inject(BreakpointObserver);
@@ -38,23 +33,7 @@ export class Layout {
   @ViewChild('main') main!: ElementRef<HTMLElement> ;
 
 
-  @HostListener('window:click', ['$event'])
-  onWindowClick(event: Event) {
-    if(!(event.target instanceof Node)) return;
-    else if (this.openMenu.nativeElement.contains(event.target as Node)) {
-      this.openMenu.nativeElement.classList.remove('chosen');
-      this.closeMenu.nativeElement.classList.add('chosen');
-      this.adaptiveMenu.nativeElement.classList.add('opened');
-    }
-    else if (this.closeMenu.nativeElement.contains(event.target as Node) || this.main.nativeElement.contains(event.target as Node)) {
-
-      this.openMenu.nativeElement.classList.add('chosen');
-      this.closeMenu.nativeElement.classList.remove('chosen');
-      this.adaptiveMenu.nativeElement.classList.remove('opened');
-    }
-  }
-
-  constructor() {
+  constructor(private translate: TranslateService) {
     this.bo.observe(['(max-width: 1440px)']).subscribe(result => {
       this.labtop.set(result.matches);
       if(result.matches) this.adaptive = true;
@@ -65,4 +44,54 @@ export class Layout {
       this.mobile.set(result.matches);
     });
   }
+
+  ngOnInit(){
+    //Language settings
+    this.chosenLanguage = localStorage.getItem("language");
+    if(this.chosenLanguage===null){
+      this.chosenLanguage = "en";
+      localStorage.setItem("language", "en");
+    }
+    this.translate.use(this.chosenLanguage); 
+  }
+  
+  onLanguageChange(){
+    localStorage.setItem("language", this.chosenLanguage!);
+    this.translate.use(this.chosenLanguage!); 
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if(pageYOffset > 440 && !this.adaptive) {
+      this.compact = true;
+    }
+    else {
+      this.compact = false;
+    }
+  }
+
+
+  @HostListener('window:click', ['$event'])
+  onWindowClick(event: Event) {
+    if(!(event.target instanceof Node)) return;
+    else{
+      if (this.openMenu.nativeElement.contains(event.target as Node)) {
+        this.openMenu.nativeElement.classList.remove('chosen');
+        this.closeMenu.nativeElement.classList.add('chosen');
+        this.adaptiveMenu.nativeElement.classList.add('opened');
+      }
+      else if (this.closeMenu.nativeElement.contains(event.target as Node) || this.main.nativeElement.contains(event.target as Node)) {
+        this.openMenu.nativeElement.classList.add('chosen');
+        this.closeMenu.nativeElement.classList.remove('chosen');
+        this.adaptiveMenu.nativeElement.classList.remove('opened');
+      }
+      if(this.openLanguageWindow.nativeElement.contains(event.target as Node)){
+        this.showLanguageWindow.set(!this.showLanguageWindow());
+      }
+      else if(!(this.languageForm.nativeElement.contains(event.target as Node))){
+        this.showLanguageWindow.set(false);
+      }
+    }
+  }
+
 }
