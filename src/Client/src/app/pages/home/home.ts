aca49df308cc881f29f83ebb7ca14767b0a4b829
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-12-22 14:31:22 +0200

    Update links

--- src/Client/src/app/pages/home/home.ts
+++ src/Client/src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { News } from '../news/news/news';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [TranslateModule, News],
+  imports: [TranslateModule, News, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
