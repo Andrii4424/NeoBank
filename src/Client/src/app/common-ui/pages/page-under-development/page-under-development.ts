89886b6ca9d27a15132d05f01b87ae15a8336d78
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-12-22 14:32:01 +0200

    Add coming soon page

---
+++ src/Client/src/app/common-ui/pages/page-under-development/page-under-development.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-page-under-development',
+  imports: [TranslateModule],
+  templateUrl: './page-under-development.html',
+  styleUrl: './page-under-development.scss'
+})
+export class PageUnderDevelopment {
+
+}
